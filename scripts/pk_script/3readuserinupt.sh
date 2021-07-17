adcb39cf333f3c084b6a29fddd1d7023687f4f44
wankhede3583 <wankhede3583@gmail.com>
2021-07-18 00:02:42 +0530

    commit form cmd

--- scripts/pk_script/3readuserinupt.sh
+++ scripts/pk_script/3readuserinupt.sh
@@ -2,12 +2,10 @@
 
 # this is script for read user input
 
-
 echo "Enter first Name : "
 read name1
 echo "Enter second Name : "
 read name2
 echo "Enter last Name : "
 read name3
-
-echo "Hello, " $name1 $name2 $name3
+echo "Hello, " $name1 $name2 $name3 " Welcome"
