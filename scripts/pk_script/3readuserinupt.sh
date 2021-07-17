92adaf7a3427e02f1b489c5180907b3a6e22c36b
wankhede3583 <wankhede3583@gmail.com>
2021-07-17 23:52:47 +0530

    add 3 script

---
+++ scripts/pk_script/3readuserinupt.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# this is script for read user input
+
+
+echo "Enter first Name : "
+read name1
+echo "Enter second Name : "
+read name2
+echo "Enter last Name : "
+read name3
+
+echo "Hello, " name1 name2 name3
