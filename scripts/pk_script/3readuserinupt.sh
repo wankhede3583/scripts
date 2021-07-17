34851fd00e60110aca7013b2d0a2143031c468e3
wankhede3583 <wankhede3583@gmail.com>
2021-07-18 00:24:15 +0530

    commit form cmd

--- scripts/pk_script/3readuserinupt.sh
+++ scripts/pk_script/3readuserinupt.sh
@@ -2,8 +2,8 @@
 
 # this is script for read user input
 # -p for show input in single line
- 
+ # -s for hide password form user
 read -p "Enter user name : " user_var 
-read -p "Enter Password : " pass_var
+read -sp "Enter Password : " pass_var
 echo "your username is : " $user_var
 echo "your password is : " $pass_var
