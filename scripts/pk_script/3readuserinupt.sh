8ec046e6016a60f33e483e4b57595fba12b87ce5
wankhede3583 <wankhede3583@gmail.com>
2021-07-18 00:11:20 +0530

    commit form cmd

--- scripts/pk_script/3readuserinupt.sh
+++ scripts/pk_script/3readuserinupt.sh
@@ -2,10 +2,9 @@
 
 # this is script for read user input
 
-echo "Enter first Name : "
-read name1
-echo "Enter second Name : "
-read name2
-echo "Enter last Name : "
-read name3
-echo "Hello, " $name1 $name2 $name3 " Welcome"
+echo "Enter user name : "
+read -p user_var
+echo "Enter Password : "
+read -p pass_var
+echo "your username is : " $user_var
+echo "your password is : " $pass_var
